73ef006eb6c76e3ce83a141cf929e21d21c1b50f
VolodymyrKaraniuk <volodimirkaranuk7@gmail.com>
2025-03-15 14:08:42 +0100

    Solution

--- task.ps1
+++ task.ps1
@@ -41,17 +41,17 @@ New-AzVm `
 
 # ↓↓↓ Write your code here ↓↓↓
 
-# $Params = @{
-#     ResourceGroupName  = $resourceGroupName
-#     VMName             = $vmName
-#     Name               = 'CustomScript'
-#     Publisher          = 'Microsoft.Azure.Extensions'
-#     ExtensionType      = 'CustomScript'
-#     TypeHandlerVersion = '2.1'
-#     ProtectedSettings  = @{
-#         fileUris = @('https://raw.githubusercontent.com/VolodymyrKaraniuk/azure_task_12_deploy_app_with_vm_extention/main/install-app.sh')
-#         commandToExecute = './install-app.sh'
-#     }
-# }
+$Params = @{
+    ResourceGroupName  = $resourceGroupName
+    VMName             = $vmName
+    Name               = 'CustomScript'
+    Publisher          = 'Microsoft.Azure.Extensions'
+    ExtensionType      = 'CustomScript'
+    TypeHandlerVersion = '2.1'
+    ProtectedSettings  = @{
+        fileUris = @('https://raw.githubusercontent.com/VolodymyrKaraniuk/azure_task_12_deploy_app_with_vm_extention/main/install-app.sh')
+        commandToExecute = './install-app.sh'
+    }
+}
 
-# Set-AzVMExtension @Params
+Set-AzVMExtension @Params
